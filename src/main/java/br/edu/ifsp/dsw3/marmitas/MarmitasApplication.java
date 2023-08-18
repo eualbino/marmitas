fea3ab3fd089bd87d7d5cbc148a6bbe194608484
Lucas Apóstolo <lucas.g.apostolo@gmail.com>
2023-08-18 10:36:45 -0300

    Marmitas

---
+++ src/main/java/br/edu/ifsp/dsw3/marmitas/MarmitasApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifsp.dsw3.marmitas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarmitasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarmitasApplication.class, args);
+	}
+
+}
